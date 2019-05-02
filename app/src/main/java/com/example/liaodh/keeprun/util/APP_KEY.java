41adcec3443fa67500f498b35bc65406dc1f9312
liaodh <2514750678@qq.com>
2019-05-02 19:59:05 +0800

    数据库

---
+++ app/src/main/java/com/example/liaodh/keeprun/util/APP_KEY.java
@@ -0,0 +1,6 @@
+package com.example.liaodh.keeprun.util;
+
+public class APP_KEY {
+    public static final String weather_key = "0d6dd334ddcf45b7bb656300d0c18aca";
+    public static final String weather_url = "http://guolin.tech/api/wather?cityid=";
+}
