677919dd21549be13fce5e12e43bd83ec504dd14
liaodh <2514750678@qq.com>
2019-05-04 21:10:39 +0800

    跑步页布局

--- app/src/main/java/com/example/liaodh/keeprun/view/RunFragment.java
+++ app/src/main/java/com/example/liaodh/keeprun/view/RunFragment.java
@@ -1,5 +1,7 @@
 package com.example.liaodh.keeprun.view;
 
+import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,11 +10,37 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.liaodh.keeprun.R;
+import com.example.liaodh.keeprun.databinding.FragmentRunBinding;
+import com.facebook.drawee.view.SimpleDraweeView;
 
 public class RunFragment extends Fragment {
 
+    private FragmentRunBinding runBinding;
+
     public View onCreateView(@Nullable LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_run, container, false);
+        return initView(inflater, container);
+    }
+
+    private View initView(LayoutInflater inflater, ViewGroup container) {
+        runBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_run, container, false);
+        initBar();
+        return runBinding.getRoot();
+
+    }
+
+    private void initBar() {
+        ColorArcProgressBar arcProgressBar;
+        arcProgressBar = runBinding.healthBar;
+        arcProgressBar.setMaxValues(100);
+        arcProgressBar.setTitle("今日步数");
+        arcProgressBar.setCurrentValues(60);
+
+        SimpleDraweeView draweeView = runBinding.userRunImage;
+        Uri uri = Uri.parse("res:///" + R.drawable.timg);
+        draweeView.setImageURI(uri);
+
+        draweeView = runBinding.startRunImage;
+        draweeView.setImageURI(uri);
     }
 }
